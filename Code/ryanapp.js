d49856eb959f8c39e4714c27f64ae806e619df52
ryancmedina <ryancmedina42@gmail.com>
2025-05-14 00:33:10 -0500

    5/13

--- Code/ryanapp.js
+++ Code/ryanapp.js
@@ -1,5 +1,6 @@
-//import jsonData from './neows_clusters.csv';
 const csvData = await importCSV('/Resources/neo_predictions.csv');
+let dropdown = d3.select("#cluster");
+let dropdown2 = d3.select("#type");
 
 async function importCSV(file) 
 {
@@ -27,12 +28,11 @@ async function importCSV(file)
   }
 }
 
-function makeChart()
+function demo()
 {
-  let x_marks = csvData.map(item => item.estimated_diameter_km_min);
-  let y_marks = csvData.map(item => item.relative_velocity_kph);
+  let x_marks = csvData.map(item => item.dm_max);
+  let y_marks = csvData.map(item => item.vel);
   let names = csvData.map(item => item.Name);
-  let colors = csvData.map(item => item.cluster)
 
   let trace  ={
     x: x_marks,
@@ -42,7 +42,7 @@ function makeChart()
     mode: "markers",
     marker: {
       size: 10,
-      color: colors,
+      color: "red",
       hovertemplate: "h"
     },
     hoverinfo: "text"
@@ -64,47 +64,188 @@ function makeChart()
     hovermode : "closest"
   }
 
+  Plotly.newPlot("demo", scatter, layout);
+}
+
+function makeChart()
+{
+  let x_marks = csvData.map(item => item.estimated_diameter_km_min);
+  let y_marks = csvData.map(item => item.relative_velocity_kph);
+  let names = csvData.map(item => item.Name);
+  let clusters = csvData.map(item => item.cluster);
+  let text = []
+  let colors = [];
+
+  for(let i = 0; i < clusters.length; i++)
+  {
+    if(clusters[i] == 0)
+    {
+      colors.push("red");
+    }
+    else if(clusters[i] == 1)
+    {
+      colors.push("orange");
+    }
+    else if(clusters[i] == 2)
+    {
+      colors.push("yellow");
+    }
+    else if(clusters[i] == 3)
+    {
+      colors.push("purple");
+    }
+    else
+    {
+      colors.push("pink");
+    }
+  }
+
+  for (let i = 0; i < names.length; i++)
+  {
+    text.push("Name: " + names[i] + "\nCluster: " + clusters[i]);
+  }
+
+  let trace  ={
+    x: x_marks,
+    y: y_marks,
+    text: text,
+    type: "scatter",
+    mode: "markers",
+    marker: {
+      size: 10,
+      color: colors,
+      hovertemplate: "h"
+    },
+    hoverinfo: "text"
+  }
+
+  let scatter = [trace];
+
+  let layout = {
+    title: "Velocity vs Diameter of Asteroids Near Earth",
+    xaxis: {
+      title : "Estimated Max Diameter (Scaled)"
+      //range: [0, 130000]
+    },
+    yaxis: {
+      title: "Relative Velocity (Scaled) "
+      //range: [0, 1.5]
+    },
+    height: 800,
+    hovermode : "closest"
+  }
+
   Plotly.newPlot("plot", scatter, layout);
 
 }
 
-function displayStats(cluster)
+function displayStats(cluster, dataset)
 {
-  //console.log(csvData[0])
-  let avg_absmag = 0;
+  let mean = 0;
   let x = 0;
+  let c = parseInt(cluster.charAt(8));
+  let d = [];
+  let unit = "KM";
+  let sorted = [];
+  let clusterData = [];
+  let median = 0;
+  let vari = 0;
 
-  for(let i = 0; i < csvData.length; i++)
+  if(dataset == "Absolute Magnitude (H)")
   {
-    if(csvData[i].cluster == cluster)
+    d = csvData.map(item => item.mag);
+    unit = "H";
+  }
+  else if(dataset == "Minimum Estimated Diameter (KM)")
+  {
+    d = csvData.map(item => item.dm_min);
+  }
+  else if(dataset == "Maximum Estimated Diameter (KM)")
+  {
+    d = csvData.map(item => item.dm_max);
+  }
+  else if(dataset == "Relative Velocity (KPH)")
+  {
+    d = csvData.map(item => item.vel);
+    unit = "KPH"
+  }
+  else
+  {
+    d = csvData.map(item => item.miss);
+  }
+
+  for(let i = 0; i < d.length; i++)
+  {
+    if(csvData[i].cluster == c)
     {
-      avg_absmag += parseFloat(csvData[i].absolute_magnitude_h);
-      //console.log(avg_absmag);
+      mean += parseFloat(d[i]);
+      clusterData.push(parseFloat(d[i]))
       x += 1;
     }
   }
-  avg_absmag = avg_absmag/x;
+  mean = mean/x;
 
-  console.log(avg_absmag);
-}
+  
+  sorted = clusterData.sort((a, b) => a - b);
+  if(c == 3)
+  {
+    median = clusterData[0];
+  }
+  else if(clusterData.length % 2 == 0)
+  {
+    median = sorted[parseInt(sorted.length / 2)];
+  }
+  else
+  {
+    median = (sorted[(Math.floor(clusterData.length / 2)) - 1] + sorted[(Math.floor(clusterData.length / 2))]) / 2
+  }
 
-function numberChanged()
-{
-  displayStats(c)
+  for(let i = 0; i < clusterData.length; i++)
+  { 
+    vari += ((clusterData[i] - mean) ** 2);
+  }
+  
+  vari = vari / clusterData.length;
+
+  let stdev = Math.sqrt(vari);
+
+  d3.select("#mean").text("Mean: " + mean.toFixed(2) + " " + unit);
+  d3.select("#median").text("Median: " + median.toFixed(2) + " " + unit);
+  d3.select("#var").text("Variance: " + vari.toFixed(2));
+  d3.select("#stdev").text("Standard Deviation: " + stdev.toFixed(2));
 }
 
 function init()
 {
-  let dropdown = d3.select("#cluster");
-  dropdown.append("option").text("Cluster 1")
-  dropdown.append("option").text("Cluster 2")
-  dropdown.append("option").text("Cluster 3")
-  dropdown.append("option").text("Cluster 4")
-  dropdown.append("option").text("Cluster 5")
+  dropdown.append("option").text("Cluster 0 (Red)");
+  dropdown.append("option").text("Cluster 1 (Orange)");
+  dropdown.append("option").text("Cluster 2 (Yellow)");
+  dropdown.append("option").text("Cluster 3 (Purple)");
+  dropdown.append("option").text("Cluster 4 (Pink)");
   
+  dropdown2.append("option").text("Absolute Magnitude (H)");
+  dropdown2.append("option").text("Minimum Estimated Diameter (KM)");
+  dropdown2.append("option").text("Maximum Estimated Diameter (KM)");
+  dropdown2.append("option").text("Relative Velocity (KPH)");
+  dropdown2.append("option").text("Miss Distance (KM)");
+
   console.log(csvData);
+  demo();
   makeChart();
-  displayStats();
+  displayStats("Cluster 0", "Absolute Magnitude (H)");
+}
+
+function numberChanged()
+{
+  displayStats(dropdown.property("value"), dropdown2.property("value"));
+}
+
+function dataChanged()
+{
+  displayStats(dropdown.property("value"), dropdown2.property("value"));
 }
 
 init();
+
+d3.selectAll("#cluster").on("change", numberChanged);
+d3.selectAll("#type").on("change", dataChanged);
